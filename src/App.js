146e951c67a8223c72bef5649e78db0baed2dbd8
sandip.tamang <sandip.tamang@bearingpoint.com>
2024-08-28 15:33:27 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import FuncCounter from './components/FuncCounter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* <ClassCounter />
+      <FuncCounter/> */}
     </div>
   );
 }
