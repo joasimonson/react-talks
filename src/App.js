14b7eb3c1b16ae54292a9fff05d456645f431499
enzo.corsini <enzo.corsini@bearingpoint.com>
2024-10-03 10:30:35 +0100

    baseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {BaseForm} from "./form/BaseForm";
-import formStore from "./stores/formStore";
-import {SummaryBase} from "./summary/SummaryBase";
 
 
 function App() {
-    const {registered, queryMessage} = formStore((state) => ({
-        registered:  state.registered,
-        queryMessage: state.queryMessage
-        //queryMessage: state.queryMessage()
-    }))
-    // debugger
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {<BaseForm/>}
-        {registered && <SummaryBase/>}
-          //{registered && (<h3>{queryMessage()}</h3>)}
-        {registered && (<h3>{queryMessage}</h3>)}
       </header>
     </div>
   );
